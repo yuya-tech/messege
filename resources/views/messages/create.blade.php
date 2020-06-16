72e52d5ffba6d98fd77d3fa443627d8425a5fae7
yuya_10969 <genmaimai140@gmail.com>
2020-06-16 23:07:27 +0900

    kaka

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-6">
             {!! Form::model($message, ['route' => 'messages.store']) !!}
-            
+            //testすると
                 <div class="form-group">
                     {!! Form::label('title', 'タイトル:') !!}
                     {!! Form::text('title', null, ['class' => 'form-control']) !!}
