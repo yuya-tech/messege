8a32d22d31a70a8a3cb345edeac5ad649c3b7679
yuya-tech <>
2020-07-19 01:40:40 +0900

    最初にマイグレーションファイルを作成したら解決

--- database/migrations/2020_07_19_012655_create_messages_table.php
+++ database/migrations/2020_07_19_012655_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTitleToMessagesTable extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class AddTitleToMessagesTable extends Migration
      */
     public function up()
     {
-        Schema::table('messages', function (Blueprint $table) {
-            $table->string('title');
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');    // content カラム追加
+            $table->timestamps();
         });
     }
 
@@ -25,8 +27,6 @@ class AddTitleToMessagesTable extends Migration
      */
     public function down()
     {
-        Schema::table('messages', function (Blueprint $table) {
-            $table->dropColumn('title');
-        });
+        Schema::dropIfExists('messages');
     }
 }
