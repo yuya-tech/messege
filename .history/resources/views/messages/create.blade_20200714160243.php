bb6be63fe7bd19a8f277a349399453b1c18e1685
yuya-tech <>
2020-07-29 14:40:20 +0900

    調整

---
+++ .history/resources/views/messages/create.blade_20200714160243.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    
+     <h1>メッセージ新規作成ページ</h1>
+
+    <div class="row">
+        <div class="col-6">
+            {!! Form::model($message, ['route' => 'messages.store']) !!}
+            //testすると
+                <div class="form-group">
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                <div class="form-group">
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
+
+<!-- ここにページ毎のコンテンツを書く -->
+
+@endsection
